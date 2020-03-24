b1b72465ce66d678d8fe124fc7887180a680c6ec
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-24 18:26:12 +0900

    ２章終了

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module Projects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false, #テストデータベースにレコードを作成するファイルの作成をスキップ
+      view_specs: false, #ビュースペックを作成しない
+      helper_specs: false, #ヘルパーファイル用のスペックを作成しない
+      routing_specs: false # config/routes.rb 用のスペックファイルの作成を省略
+      # controller_specs: false コントローラスペックの生成をスキップ
+    end
   end
 end
